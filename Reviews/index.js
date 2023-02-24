98808d84d69030e704f207e232efcc1bd2cd73f4
SimoBekacem <125382745+SimoBekacem@users.noreply.github.com>
2023-02-24 17:15:48 +0100

    changes are added

---
+++ Reviews/index.js
@@ -0,0 +1,25 @@
+let personne1 = {
+    image: 'src="/javascript-project/Reviews/images/person-1_rfzshl.jpg"',
+    name: "Susan Smith",
+    job: "WEB DEVELOPER",
+    comment: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
+}
+let  personne2 = {
+    image: 'src="/javascript-project/Reviews/images/person-2_rfzshl.jpg"',
+    name: "Anna Johnson",
+    job: "WEB DEVELOPER",
+    comment: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal."
+}
+let  personne3 = {
+    image: 'src="/javascript-project/Reviews/images/person-3_rfzshl.jpg"',
+    name: "Peter Jones",
+    job: "INTERN",
+    comment: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag."
+}
+let  personne4 = {
+    image: 'src="/javascript-project/Reviews/images/person-4_rfzshl.jpg"',
+    name: "Bill Anderson",
+    job: "THE BOSS",
+    comment: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic."
+}
+let list = [personne1,personne2,personne3,personne4];
